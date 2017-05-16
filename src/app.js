71fad10bbbd6e0d11a35628c61ebd13d5a4315ba
Julian Garcia <jugar28@gmail.com>
2017-05-16 11:01:48 -0400

    basic react template updated

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import HeaderMenu from 'HeaderMenu.js';
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-               <h1 className="titulo">It is working!</h1>
-            </div>
-        )
+
+class App extends Component {
+	constructor(){
+		super();
+		this.state()={
+			displayHeaderMenu:true,
+		};
+	}
+
+    render(){
+    	let displayHeader= "";
+    	if(this.state.displayHeaderMenu === true){
+    		showGame = (
+    			<HeaderMenu />
+    		)
+    	}
+
+    	return (
+    	    {showGame}
+    	)
     }
 }
 ReactDOM.render(<App />, document.getElementById('app'));
---
+++ src/components/HeaderMenu.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class HeaderMenu extends Component{
+	render(){
+		return(
+			<header className="header">
+				<div></div>
+				<div></div>
+			</header>
+		)
+	}
+}
